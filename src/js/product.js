4d1a87dce9ee9b091dc446680939cd6c32fbb785
Spackalicious <49924555+Spackalicious@users.noreply.github.com>
2023-04-26 19:05:12 -0600

    Initial commit

---
+++ src/js/product.js
@@ -0,0 +1,16 @@
+import { setLocalStorage } from "./utils.mjs";
+import { findProductById } from "./productData.mjs";
+
+function addProductToCart(product) {
+  setLocalStorage("so-cart", product);
+}
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
+
+// add listener to Add to Cart button
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
---
+++ src/js/productData.mjs
@@ -0,0 +1,18 @@
+function convertToJson(res) {
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error("Bad Response");
+  }
+}
+
+export function getData(category = "tents") {
+  return fetch(`../json/${category}.json`)
+    .then(convertToJson)
+    .then((data) => data);
+}
+
+export async function findProductById(id) {
+  const products = await getData();
+  return products.find((item) => item.Id === id);
+}
