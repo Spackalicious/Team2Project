0a057d5e9383ec37e093719e1668f87c4bfe4bdd
Julie Spackman <spack84@byui.edu>
2023-05-17 18:29:01 -0600

    incomplete remove feature, pushing branch pre meet

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,27 +1,31 @@
-import { getLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   if (cartItems != null) {
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
     document.querySelector(".product-list").innerHTML = htmlItems.join("");
+    document.querySelector("#removeFromCart").addEventListener("click", () => remove());
   }
 }
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
+    <a href="#" class="cart-card__image">
+      <img
+        src="${item.Image}"
+        alt="${item.Name}"
+      />
+    </a>
+  <div"></div>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>    
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>    
+    <p class="cart-card__quantity">qty: 1</p>   
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+
+    <button id="removeFromCart" data-id="${item.Id}" style="margin:-50px 0 10px 300px;">X</button>    
 </li>`;
 
   return newItem;
@@ -46,5 +50,33 @@ function cartTotal() {
   }
 }
 
+function remove(item) {
+  let cartList = getLocalStorage("so-cart") || [];
+  // console.log(cartList);
+  
+  // let itemToRemove = cartList.find(cartList.ID == productID);
+  // cartList.pop(productID);
+
+  console.log(cartList);
+  
+
+  // document.querySelector("#removeFromCart").addEventListener("click", function() {
+    let productID = item.getAttribute("data-id");
+    // console.log(`This is the product ID to remove: ${productID}`);
+    cartList.pop(productID);
+    setLocalStorage("so-cart", cartList);
+
+    // pull id of item, pull contents of cart, remove item, restore cart in localStorage.
+    // vv this doesn't do that.
+    // localStorage.removeItem(cartList[productID]);
+  // })
+  
+  // function removeFromCart() {
+  //   let cart = getLocalStorage("so-cart") || [];
+    
+  // }
+}
+
 renderCartContents();
 cartTotal();
+remove();
