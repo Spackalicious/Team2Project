8c4fb5ed82d98d131eab58e53a3383f4dcc1defe
Julie Spackman <spack84@byui.edu>
2023-06-19 21:24:31 -0600

    Modal/Banner complete, but needs registration link

--- src/js/main.js
+++ src/js/main.js
@@ -19,4 +19,26 @@ function signUp() {
   }
 }
 
+function modalBanner() {
+  var modal = document.getElementById("myModal");
+  window.addEventListener("load", openModal);
+
+  function openModal() {
+    modal.style.display = "block";	
+  }
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+
+modalBanner();
 loadHeaderFooter();
